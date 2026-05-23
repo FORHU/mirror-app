eb5d30f44c43d41b5b7d66c9fbe8fbfc2caa36b9
maegju <clydeantonio.work@gmail.com>
2026-05-23 10:58:09 +0800

    stash1

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL ?? "")
   .replace(/\/$/, "");
 
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   allowedDevOrigins: ["localhost:3000", "192.168.100.9:3000"],
   async rewrites() {
